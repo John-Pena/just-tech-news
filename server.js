4f9217899c396b4091e479b8765a9341885a7f41
John-Pena <jmp1495@gmail.com>
2022-05-09 19:12:51 -0500

    fixed some server errors to allow webpage to display

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exhbs.create({})
+const hbs = exphbs.create({})
 
 const app = express();
 const PORT = process.env.PORT || 3001;
